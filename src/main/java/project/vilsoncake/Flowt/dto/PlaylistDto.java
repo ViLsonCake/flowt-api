4eab114e06066a2ceedc85a7d9186700c71fc0e9
ViLsonCake <timofeydmitrenko13@gmail.com>
2023-12-09 23:28:28 +0100

    modify playlist dto

--- src/main/java/project/vilsoncake/Flowt/dto/PlaylistDto.java
+++ src/main/java/project/vilsoncake/Flowt/dto/PlaylistDto.java
@@ -12,6 +12,7 @@ public class PlaylistDto {
     private Long id;
     private String name;
     private List<SongDto> songs;
+    private Long saveCount;
     private boolean isPrivate;
     private String username;
 
@@ -22,6 +23,7 @@ public class PlaylistDto {
         playlistDto.setPrivate(playlistEntity.isPrivate());
         playlistDto.setUsername(playlistEntity.getUser().getUsername());
         playlistDto.setSongs(playlistEntity.getSongs().stream().map(SongDto::fromSongEntity).toList());
+        playlistDto.setSaveCount((long) playlistEntity.getSavedPlaylistEntities().size());
 
         return playlistDto;
     }
