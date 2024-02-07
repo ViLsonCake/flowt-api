f485a100fa923d53d9d16ecb51c23e6cefe88fbe
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-02-07 01:20:29 +0100

    add limit to sql request

--- src/main/java/project/vilsoncake/Flowt/repository/ListenedRepository.java
+++ src/main/java/project/vilsoncake/Flowt/repository/ListenedRepository.java
@@ -13,12 +13,14 @@ public interface ListenedRepository extends CrudRepository<ListenedEntity, Long>
     @Query(value = "SELECT genre " +
             "FROM listened WHERE user_listened_statistics_id = :statisticId " +
             "GROUP BY genre " +
-            "ORDER BY count(genre) DESC", nativeQuery = true)
+            "ORDER BY count(genre) " +
+            "DESC LIMIT 5", nativeQuery = true)
     List<Genre> getMostListenedGenres(Long statisticId);
     @Query(value = "SELECT author " +
             "FROM listened WHERE user_listened_statistics_id = :statisticId " +
             "GROUP BY author " +
-            "ORDER BY count(author) DESC ", nativeQuery = true)
+            "ORDER BY count(author) " +
+            "DESC LIMIT 5", nativeQuery = true)
     List<String> getMostListenedArtists(Long statisticId);
     @Query(value = "SELECT COUNT(*) FROM listened l WHERE l.user_listened_statistics_id = :userStatistic.getId()", nativeQuery = true)
     Integer getListensCount(UserListenedStatisticEntity userStatistic);
