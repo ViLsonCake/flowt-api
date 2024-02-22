05e4518e9ecf6e55fe2ac064354ac043a5714164
ViLsonCake <timofeydmitrenko13@gmail.com>
2024-02-22 02:31:48 +0100

    refactor cookie

--- src/main/java/project/vilsoncake/Flowt/service/impl/TokenServiceImpl.java
+++ src/main/java/project/vilsoncake/Flowt/service/impl/TokenServiceImpl.java
@@ -1,16 +1,19 @@
 package project.vilsoncake.Flowt.service.impl;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 import project.vilsoncake.Flowt.entity.UserEntity;
 import project.vilsoncake.Flowt.property.JwtProperties;
 import project.vilsoncake.Flowt.repository.TokenRepository;
 import project.vilsoncake.Flowt.service.TokenService;
 import project.vilsoncake.Flowt.service.UserService;
+
+import java.time.Duration;
 
 @Service
 @Slf4j
@@ -26,20 +29,19 @@ public class TokenServiceImpl implements TokenService {
         UserEntity user = userService.getUserByUsername(username);
         user.getToken().setToken(token);
 
-        // Add refresh token to http-only cookie
-        Cookie tokenCookie = new Cookie("refreshToken", token);
-        tokenCookie.setMaxAge(daysToSeconds(jwtProperties.getRefreshLifetime()));
-        tokenCookie.setHttpOnly(true);
-        tokenCookie.setPath("/auth");
-        response.addCookie(tokenCookie);
+        ResponseCookie responseCookie = ResponseCookie.from("refreshToken", token)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/auth")
+                .maxAge(Duration.ofDays(jwtProperties.getRefreshLifetime()))
+                .build();
+
+        response.setHeader(HttpHeaders.SET_COOKIE, responseCookie.toString());
     }
 
     @Override
     public boolean isTokenExists(String token) {
         return tokenRepository.existsByToken(token);
-    }
-
-    private Integer daysToSeconds(Integer days) {
-        return days * 24 * 60 * 60;
     }
 }
